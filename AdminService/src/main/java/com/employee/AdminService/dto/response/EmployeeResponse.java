14a26d701a49b0b3ce68b41a03cc0672ba7d188d
Dhanush K <dhanush23702@gmail.com>
2026-07-21 18:06:41 +0530

    Need to setup the swagger and to be worked on the leaver service

---
+++ AdminService/src/main/java/com/employee/AdminService/dto/response/EmployeeResponse.java
@@ -0,0 +1,32 @@
+package com.employee.EmployeeProfileService.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeResponse {
+    private String employeeName;
+    private String employeeId;
+    private String contact;
+    private String emailId;
+    private String employeeDesignation;
+    private String dailyStatus;
+    private String role;
+    private String gender;
+//    private String employeeProfilePath;
+    private String permanentAddress;
+    private String currentAddress;
+    private String dateOfBirth;
+    private String emergencyContact;
+    private Long workTypeId;
+    private String profileStatus;
+    private String attendanceStatus;
+    private Long employeeStatusId;
+    private OfficeResponse office;
+    private String image;
+}
