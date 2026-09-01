4d8fcd4e1d1f6bb99698033b8e94afbcd6785368
harikrishna <harikrishna.p@optipace.in>
2026-09-01 11:24:21 +0530

    communication service spring security changes

--- CommunicationService/src/main/java/com/employee/CommunicationService/config/SecurityConfig.java
+++ CommunicationService/src/main/java/com/employee/CommunicationService/config/SecurityConfig.java
@@ -1,20 +1,29 @@
 package com.employee.CommunicationService.config;
 
+import com.employee.CommunicationService.filter.JwtAuthenticationFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.server.WebFilter;
 
 @Configuration
 @Slf4j
 @EnableWebFluxSecurity
 
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
         http
@@ -26,7 +35,7 @@ public class SecurityConfig {
                                 "/webjars/**",
                                 "/swagger-ui/**"
                         ).permitAll()
-                        .anyExchange().authenticated()
+                        .anyExchange().permitAll()
                 );
         log.info("Communication service security filter chain");
         return http.build();
--- CommunicationService/src/main/java/com/employee/CommunicationService/util/JwtUtil.java
+++ CommunicationService/src/main/java/com/employee/CommunicationService/util/JwtUtil.java
@@ -3,22 +3,57 @@ package com.employee.CommunicationService.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Component
+@Slf4j
 public class JwtUtil {
 
     @Value("${jwt.secret}")
-    private String secretKet;
+    private String secretKey;
 
-    private SecretKey getSignKey(){
-        return Keys.hmacShaKeyFor(secretKet.getBytes());
+    @Value(("${jwt.expirationMs}"))
+    private long expirationMs;
+
+//    private long expirationMs = 60000;
+
+    public SecretKey getSignKey(){
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 
-    private Claims extractAllClaims(String token){
+    public boolean validateToken(final String token){
+        try{
+            Claims claims = Jwts.parser()
+                    .verifyWith(getSignKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            Date issuedAt = claims.getIssuedAt();
+            if(issuedAt == null){
+                log.warn("JWT token is missing the IssuedAt claims");
+                return false;
+            }
+
+            long expirationTimeMs = (issuedAt.getTime() + expirationMs);
+            if(System.currentTimeMillis() > expirationTimeMs){
+                log.info("JWT token validation failed : Token expired");
+                return false;
+            }
+            return true;
+        }catch (Exception ex){
+            log.error("JWT Token signature verification or structural parsing failed", ex);
+            return false;
+        }
+    }
+
+    public Claims extractClaims(String token){
         return Jwts.parser()
                 .verifyWith(getSignKey())
                 .build()
@@ -26,14 +61,28 @@ public class JwtUtil {
                 .getPayload();
     }
 
-    public String extractEmployeeId(String token){
-        return extractAllClaims(token).get("employeeId", String.class);
+    public String extractUsername(String token){
+        return extractClaims(token).getSubject();
     }
 
-    public void validateToken(final String token){
-        Jwts.parser()
-                .verifyWith(getSignKey())
-                .build()
-                .parseSignedClaims(token);
+    public String extractEmailId(String token){
+        return extractClaims(token).get("emailId",String.class);
     }
+
+    public String extractUserId(String token){
+        return extractClaims(token).get("userId", String.class);
+    }
+
+    public String extractRole(String token) {
+        return extractClaims(token).get("role", String.class);
+    }
+
+    public String extractEmployeeId(String token){
+        return extractClaims(token).get("employeeId", String.class);
+    }
+
+    public String extractId(String token){
+        return extractClaims(token).get("id", String.class);
+    }
+
 }
